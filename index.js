f64fe6739000b9863c814a806bcc120f8fbda387
edvard01 <frejaclueless@gmail.com>
2024-01-22 16:36:11 +0100

    first three assignments

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const currentElement = document.querySelector(".list");
 
+function firstAppend() {
+  const firstNewEl = document.createElement("p");
+  const firstNewElText = document.createTextNode("This is added");
+  firstNewEl.appendChild(firstNewElText);
+
+  currentElement.append(firstNewEl);
+}
+
+function secondAppend() {
+  const newElement = document.createElement("p");
+  const newElementText = document.createTextNode("second text append");
+
+  newElement.appendChild(newElementText);
+
+  currentElement.insertAdjacentElement("beforeend", newElement);
+}
+
+function thirdAssignment() {
+  const innerHtml = currentElement.innerHTML;
+  console.log(innerHtml);
+}
+
+firstAppend();
+secondAppend();
+thirdAssignment();
