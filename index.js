7791acb0ba638dbb5d6a5f4b8dd24f465ab3497e
edvard01 <frejaclueless@gmail.com>
2024-01-23 15:34:30 +0100

    Finished the assignments

--- index.js
+++ index.js
@@ -33,14 +33,57 @@ function htmlString() {
 }
 
 function addingThroughLoop() {
-  const arr = ["Bungie jump", "Go vegan for a month", "Learn to play guitar"];
+  const arr = ["Bungie jump", "Learn to play guitar", "Go vegan for a month"];
   arr.forEach((element) => {
     currentElement.innerHTML += `<p>${element}</p>`;
   });
 }
+
+function countListItems() {
+  const children = currentElement.children;
+  console.log(`${children.length} list items.`);
+}
+
+function changeOwner() {
+  const owner = document.querySelector(".owner");
+  owner.innerHTML = "Johan's";
+}
+
+function replaceFirstItem() {
+  const children = currentElement.children;
+  const newElement = document.createElement("p");
+  const newItem = document.createTextNode("Give a million dollars to charity");
+  newElement.appendChild(newItem);
+  currentElement.replaceChild(newElement, children[0]);
+}
+
+function replaceMiddleItem() {
+  const children = document.querySelector(".list").children;
+  const newElement = document.createElement("p");
+  const newItem = document.createTextNode("Climb Sweden's tallest mountain");
+  newElement.appendChild(newItem);
+
+  if (children.length % 2 === 1) {
+    let index = (children.length + 1) / 2;
+    currentElement.replaceChild(newElement, children[index - 1]);
+  } else {
+    let index = children.length / 2;
+    currentElement.replaceChild(newElement, children[index - 1]);
+  }
+}
+
+function removeLastItem() {
+  const lastItem = document.querySelector(".list").lastChild;
+  currentElement.removeChild(lastItem);
+}
+
 firstAppend();
 secondAppend();
 thirdAssignment();
-
 htmlString();
 addingThroughLoop();
+countListItems();
+changeOwner();
+replaceFirstItem();
+replaceMiddleItem();
+removeLastItem();
