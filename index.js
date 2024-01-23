7f3420e8d550edaef696a0b610b10a8bda23e722
edvard01 <frejaclueless@gmail.com>
2024-01-23 14:06:28 +0100

    Implemented the simple append exercises

--- index.js
+++ index.js
@@ -22,6 +22,23 @@ function thirdAssignment() {
   console.log(innerHtml);
 }
 
+function htmlString() {
+  const htmlListItem = "<p>added list item nr 3</p>";
+
+  //doing currentElement.innerHTML = htmlListItem; will replace all the current text in the element.
+  //Instead we could try using a += sign, to append the html text to the existing text. Like this:
+  currentElement.innerHTML += htmlListItem;
+}
+
+function addingThroughLoop() {
+  const arr = ["Bungie jump", "Go vegan for a month", "Learn to play guitar"];
+  arr.forEach((element) => {
+    currentElement.innerHTML += `<p>${element}</p>`;
+  });
+}
 firstAppend();
 secondAppend();
 thirdAssignment();
+
+htmlString();
+addingThroughLoop();
